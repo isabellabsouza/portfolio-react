a369758c5f04f9559ce29deed4e6af2f6a7a16a0
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2024-11-25 15:51:24 -0300

    education section

--- src/components/Sections/Education/EducationCard.jsx
+++ src/components/Sections/Education/EducationCard.jsx
@@ -1,61 +1,48 @@
-import Typography from '@mui/material/Typography';
+import { Card, Divider, Typography } from '@mui/material';
+import { motion } from "framer-motion";
 import Grid from '@mui/material/Grid2';
-import Card from '@mui/material/Card';
-import Divider from '@mui/material/Divider';
-import { motion } from "framer-motion"
 
 export default function EducationCard({ education }) {
 
     return (
-            <Card
-                variant='outlined'
-                sx={{
-                    paddingInline: 3,
-                    paddingY: 2,
-                    borderRadius: 0,
-                    marginBottom: 6,
-                }}
+        <Grid size={6}>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                viewport={{ once: false, amount: 0.3 }}
             >
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
-                    viewport={{ once: false, amount: 0.3 }}
+                <Card
+                    variant='outlined'
+                    sx={{
+                        paddingInline: 3,
+                        paddingY: 2,
+                        borderRadius: 0,
+                        marginBottom: 6,
+                    }}
                 >
+
                     <Typography variant="body2" >
                         {education.duration}
                     </Typography>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.6 }}
-                    viewport={{ once: false, amount: 0.3 }}
-                >
+
                     <Typography variant="h6">
                         {education.type}
                     </Typography>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.7 }}
-                    viewport={{ once: false, amount: 0.3 }}
-                >
+
                     <Typography variant="body1">
                         {education.institution}
                     </Typography>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8 }}
-                    viewport={{ once: false, amount: 0.3 }}
-                >
+
                     <Typography variant="body1" color='pink'>
                         {education.title}
                     </Typography>
-                </motion.div>
-            </Card>
+                    <Divider sx={{ marginY: 2 }} />
+                    <Typography variant='body2'>
+                        {education.description}
+                    </Typography>
+                </Card>
+            </motion.div>
+        </Grid>
     )
 }
--- src/components/Sections/Education/index.jsx
+++ src/components/Sections/Education/index.jsx
@@ -1,33 +1,102 @@
 import educationData from './assets/educationData.json';
 import EducationCard from './EducationCard';
 import Grid from '@mui/material/Grid2';
-import { Typography, Box } from '@mui/material';
-import DeskModel from './DeskModel';
+import { Typography, Box, Card, CardMedia, CardContent, Avatar } from '@mui/material';
+import AwsBadge from '/assets/aws-badge.png';
+import KoreaFlag from '/assets/korea-flag.png';
+import UsaFlag from '/assets/usa-flag.jpg';
+import { motion } from "framer-motion";
 
 export default function Education() {
     return (
         <Box
             id="projects"
             sx={{
-                height: '92vh',
+                height: '95vh',
                 paddingTop: '50px',
                 textAlign: 'center',
             }}
         >
             <Typography variant="h4" sx={{ marginBottom: 5 }} >Formações</Typography>
-            <Grid container spacing={6} >
-                <Grid size={7} >
-                    <DeskModel />
-                </Grid>
-                <Grid size={5}>
-                    {
-                        educationData.map((education) => (
-                            <EducationCard
-                                key={education.institution}
-                                education={education}
+            <Grid container spacing={5} >
+                {
+                    educationData.map((education) => (
+                        <EducationCard
+                            key={education.institution}
+                            education={education}
+                        />
+                    ))
+                }
+            </Grid>
+            <Grid container spacing={5} >
+                <Grid size={6} >
+                    <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                        viewport={{ once: false, amount: 0.3 }}
+                    >
+                        <Card variant='outlined' sx={{ display: 'flex', borderRadius: 0 }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 200 }}
+                                image={AwsBadge}
+                                alt="Live from space album cover"
                             />
-                        ))
-                    }
+                            <CardContent>
+                                <Typography variant="h6" sx={{ padding: 2 }} color='pink'>
+                                    AWS Cloud Foundations
+                                </Typography>
+                                <Typography variant="body2" sx={{ padding: 2 }}>
+                                    Curso introdutório com a visão geral detalhada de conceitos de nuvem, produtos essenciais da AWS, segurança, arquitetura, preço e suporte.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </motion.div>
+                </Grid>
+                <Grid size={6} >
+                    <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                        viewport={{ once: false, amount: 0.3 }}
+                    >
+                        <Card variant='outlined' sx={{ borderRadius: 0, paddingBottom: 2 }}>
+                            <CardContent>
+                                <Typography variant="h6" sx={{ padding: 2 }} color='pink'>
+                                    Idiomas
+                                </Typography>
+                                <Grid container spacing={2} sx={{ justifyContent: 'center', marginTop: 3 }}>
+                                    <Grid size={2} />
+                                    <Grid size={4} sx={{ display: 'flex', alignItems: 'center' }}>
+
+                                        <Avatar src={KoreaFlag} sx={{ width: 56, height: 56 }} />
+                                        <Box sx={{ paddingLeft: 2, textAlign: 'start' }}>
+                                            <Typography variant="body1">
+                                                Coreano
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                Intermediário
+                                            </Typography>
+                                        </Box>
+                                    </Grid>
+                                    <Grid size={4} sx={{ display: 'flex', alignItems: 'center' }}>
+
+                                        <Avatar src={UsaFlag} sx={{ width: 56, height: 56 }} />
+                                        <Box sx={{ paddingLeft: 2, textAlign: 'start' }}>
+                                            <Typography variant="body1">
+                                                Inglês
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                Avançado
+                                            </Typography>
+                                        </Box>
+                                    </Grid>
+                                    <Grid size={2} />
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </motion.div>
                 </Grid>
             </Grid>
 
